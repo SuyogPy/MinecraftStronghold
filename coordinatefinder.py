da9618379e6ab7849c8316b982a5a5f2d8646d1c
Suyog Pandey <suyogpandey206554@gmail.com>
2026-03-14 17:11:26 +0545

    added function getseed

--- coordinatefinder.py
+++ coordinatefinder.py
@@ -13,8 +13,17 @@ def storeseedandcoords():
 
 def create_new_world():
     pyautogui.click()#coordinate to create new world button TODO: ADD COORD
-    pyautogui.click()#coordinate to create new world button TODO: ADD COORD
+    time.sleep(3)#Wait for menu to load
+    pyautogui.click()#coordinate to Allow commands button TODO: ADD COORD
+    pyautogui.click()#coordinate to create world button TODO: ADD COORD
+    time.sleep(40)#Wait for world to load
 
+def getseed():
+    pyautogui.press("/")#open chat
+    pyautogui.typewrite("seed")#type command to get seed
+    pyautogui.press("enter")#execute command
+    time.sleep(1)#Wait for chat to update
+    pyautogui.click()#Coordinate to the seed TODO: ADD COORD
 
 def main():
     time.sleep(5)
