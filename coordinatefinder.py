ad2422217edf65d269d2c87eaa13dc913d38cff2
Suyog Pandey <suyogpandey206554@gmail.com>
2026-03-14 17:02:18 +0545

    fix: fixed the order of definition of functions

--- coordinatefinder.py
+++ coordinatefinder.py
@@ -5,20 +5,22 @@ import time
 SEED = ""
 COORDS = ""
 
-if __name__ == "__main__":
-    main()
 
-def main():
-    time.sleep(5)
-    pyautogui.alertprompt("Please Make sure Minecraft is running and you are on the songle player menu.")
-    
+def storeseedandcoords():
+    with open("data.txt", "w") as f:
+        f.write(SEED + "," + str(COORDS))
+
 
 def create_new_world():
     pyautogui.click()#coordinate to create new world button TODO: ADD COORD
     pyautogui.click()#coordinate to create new world button TODO: ADD COORD
 
 
-def storeseedandcoords():
-    with open("data.txt", "w") as f:
-        f.write(SEED + "," + str(COORDS))
+def main():
+    time.sleep(5)
+    pyautogui.alertprompt("Please Make sure Minecraft is running and you are on the songle player menu.")
 
+if __name__ == "__main__":
+    main()
+else:
+    print("This module is not meant to be imported. So... Nice try kiddo.")
