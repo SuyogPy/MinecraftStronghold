0ba6d83ac28ccc2463da29111651a18031bb361a
Suyog Pandey <suyogpandey206554@gmail.com>
2026-03-15 13:29:22 +0545

    added getstronghold function and fixed minor bugs

--- coordinatefinder.py
+++ coordinatefinder.py
@@ -25,28 +25,44 @@ seed_coord={
     "y": 0
 }#TODO: ADD COORDS FOR SEED BUTTON
 
-
+str_coord={
+    "x": 0,
+    "y": 0
+}#TODO: ADD COORDS FOR STRONGHOLD COORDS IN CHAT
 
 def storeseedandcoords():
-    with open("data.txt", "w") as f:
+    with open("data.txt", "a") as f:
         f.write(SEED + "," + str(COORDS))
 
 
 def create_new_world():
-    pyautogui.click(create_world.x, create_world.y)#coordinate to create new world button TODO: ADD COORD
+    pyautogui.click(create_world["x"], create_world["y"])#coordinate to create new world button TODO: ADD COORD
     time.sleep(3)#Wait for menu to load
-    pyautogui.click(allow_commands.x, allow_commands.y)#coordinate to Allow commands button TODO: ADD COORD
-    pyautogui.click(create_new_world_btn.x, create_new_world_btn.y)#coordinate to create world button TODO: ADD COORD
+    pyautogui.click(allow_commands["x"], allow_commands["y"])#coordinate to Allow commands button TODO: ADD COORD
+    pyautogui.click(create_new_world_btn["x"], create_new_world_btn["y"])#coordinate to create world button TODO: ADD COORD
     time.sleep(40)#Wait for world to load
+
+def getsronghold():
+    pyautogui.press("/")#open chat
+    pyautogui.typewrite("locate structure minecraft:stronghold")#type command to get stronghold coords
+    pyautogui.press("enter")#execute command
+    time.sleep(1)#Wait for chat to update
+    pyautogui.click(str_coord["x"], str_coord["y"])#Coordinate to the stronghold coords TODO: ADD COORD
+    pyautogui.hotkey("ctrl", "a")#Select stronghold coords
+    pyautogui.hotkey("ctrl", "c")#Copy stronghold coords to clipboard
+    global COORDS 
+    COORDS = pyperclip.paste()#Copy coords to clipboard and store in variable
 
 def getseed():
     pyautogui.press("/")#open chat
     pyautogui.typewrite("seed")#type command to get seed
     pyautogui.press("enter")#execute command
     time.sleep(1)#Wait for chat to update
-    pyautogui.click(seed_coord.x, seed_coord.y)#Coordinate to the seed TODO: ADD COORD
+    pyautogui.click(seed_coord["x"], seed_coord["y"])#Coordinate to the seed TODO: ADD COORD
     global SEED 
     SEED = pyperclip.paste()#Copy seed to clipboard and store in variable
+    getsronghold()#Get stronghold coords after getting seed
+    
 
 def main():
     time.sleep(5)
