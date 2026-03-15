806add4e2c73c1a7857518957515a995dd84cb8a
Suyog Pandey <suyogpandey206554@gmail.com>
2026-03-15 13:29:40 +0545

    added a todo

--- pointer.py
+++ pointer.py
@@ -4,4 +4,4 @@ import time
 
 
 def main():
-    pyautogui.alert("This is made so that you can figure out the coordinate of Minecraft putton to put in coordinatefinder.py")
+    pyautogui.alert("This is made so that you can figure out the coordinate of Minecraft putton to put in coordinatefinder.py")#TODO: Yet to be completed.
